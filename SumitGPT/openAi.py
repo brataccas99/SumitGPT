843064748006ae1da3cbcf7e82f24008412a75d9
gabriele <gabriele.lodato@itsvil.it>
2023-03-10 13:54:46 +0100

    [Sumit-gpt-003] finally working tokens handling

--- SumitGPT/openAi.py
+++ SumitGPT/openAi.py
@@ -29,10 +29,13 @@ def openAiCallSummary(text: str, prompt: str):
     openai.api_key = get_api_key("C:/Users/roach/Desktop/openAI_api_key.txt")
     # Use the GPT-3 API to generate a summary
     model_engine = "text-davinci-003"
-    encoding = tiktoken.model.get_encoding("cl100k_base"), tiktoken.model.encoding_for_model("gpt-3.5-turbo")
+    encoding = tiktoken.get_encoding("cl100k_base")
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     prompt_to_send = f"{prompt} {text}"
-    tokens = 4087 - len(encoding.encode(text))
-    print("lunghezza testo: ", len(text), "\n lunghezza tokens: ", len(encoding.encode(text)), "\n testo: ", text)
+    encoding.encode(prompt_to_send)
+    tokens = 4000 - len(encoding.encode(prompt_to_send)) - int((len(encoding.encode(prompt_to_send)) * 0.3))
+    print(tokens)
+    print("lunghezza testo: ", len(text), "\n lunghezza tokens: ", len(encoding.encode(prompt_to_send)), "\n testo: ", text)
     try:
         completions = openai.Completion.create(engine=model_engine, prompt=prompt_to_send,
                                                max_tokens=tokens,
@@ -54,11 +57,11 @@ def splitString(test):
 
 
 def checkLength(text, prompt):
-    max_tokens = 4087
+    max_tokens = 4000
     encoding = tiktoken.get_encoding("cl100k_base")
     encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     encoding.encode(text)
-    if (max_tokens - len(encoding.encode(text))) <= 0:
+    if (max_tokens - len(encoding.encode(text + prompt))) <= 0:
         result = splitString(text)
         return [openAiCallSummary(s, prompt) for s in result]
     else:
@@ -71,7 +74,7 @@ def openAIService(text: list,
     result = ""
     if len(text) > 1:
         for i in text:
-            backValue = checkLength(text[i], prompt)
+            backValue = checkLength(i, prompt)
             if isinstance(backValue, list):
                 for b in backValue:
                     listBack.append(b)
@@ -87,6 +90,5 @@ def openAIService(text: list,
 
     for i in range(len(listBack)):
         result += ' ' + listBack[i]
-    print(result)
 
     return result
